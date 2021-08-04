5135fefd19ee3015f016e373f3a6352d82e04794
Preeti Rawat <preetirawat711@gmail.com>
2021-08-04 16:01:21 +0530

    Java Primality Test

---
+++ src/Java/Easy/PrimalityTest.java
@@ -0,0 +1,21 @@
+package Java.Easy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class PrimalityTest {
+    public static void main(String[] args) throws IOException {
+
+        Scanner sc=new Scanner(System.in);
+        BigInteger n=sc.nextBigInteger();
+        if(n.isProbablePrime(100)){
+            System.out.println("prime");
+        }else{
+            System.out.println("not prime");
+        }
+        sc.close();
+    }
+}
