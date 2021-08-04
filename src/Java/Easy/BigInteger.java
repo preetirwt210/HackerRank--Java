751574e40d744b6f9f11355c36c18331622164ad
Preeti Rawat <preetirawat711@gmail.com>
2021-08-04 16:11:05 +0530

    Java BigInteger

---
+++ src/Java/Easy/BigInteger.java
@@ -0,0 +1,16 @@
+package Java.Easy;
+
+import java.util.Scanner;
+
+public class BigInteger {
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc=new Scanner(System.in);
+        java.math.BigInteger a=sc.nextBigInteger();
+        java.math.BigInteger b=sc.nextBigInteger();
+        System.out.println(a.add(b));
+        System.out.println(a.multiply(b));
+
+
+    }
+}
