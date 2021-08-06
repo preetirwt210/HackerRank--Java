0234edf2e67d84b5d3a9d9653a04db3011a8c995
Preeti Rawat <preetirawat711@gmail.com>
2021-08-06 16:16:37 +0530

    Java Exception Handling (Try-catch)

---
+++ src/Java/Easy/ExceptionHandling.java
@@ -0,0 +1,23 @@
+package Java.Easy;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class ExceptionHandling {
+    public static void main(String[] args) {
+        /* Enter your code here.
+         Read input from STDIN. Print output to STDOUT.
+         Your class should be named Solution. */
+        Scanner sc=new Scanner(System.in);
+        int x,y;
+        try{
+            x=sc.nextInt();
+            y= sc.nextInt();
+            System.out.println(x/y);
+        }catch(InputMismatchException e){
+            System.out.println("java.util.InputMismatchException");
+        }catch (Exception e){
+            System.out.println(e);
+        }
+    }
+}
