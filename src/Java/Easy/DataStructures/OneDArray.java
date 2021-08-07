6171a2eaaf93efbb7235592d2c08f78b95fb7a4b
Preeti Rawat <preetirawat711@gmail.com>
2021-08-07 17:10:23 +0530

    Java 1D Array

---
+++ src/Java/Easy/DataStructures/OneDArray.java
@@ -0,0 +1,23 @@
+package Java.Easy.DataStructures;
+
+import java.util.Scanner;
+
+public class OneDArray {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int[] a=new int[n];
+
+        for(int i=0;i<n;i++){
+           int val=scan.nextInt();
+            a[i]=val;
+        }
+        scan.close();
+
+        // Prints each sequential element in array a
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
+        }
+    }
+}
